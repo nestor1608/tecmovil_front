3ab978d2b2610e422546c5d9992583914d203fc1
nestor16 <nestor_gentil@hotmail.com>
2025-05-09 18:14:45 -0300

    update change in url form

--- src/services/productServices.js
+++ src/services/productServices.js
@@ -15,7 +15,8 @@ const shopifyClient = axios.create({
 });
 
 // Configuración de la API local
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://tec-movilback-y99h.onrender.com';
+const API_URL = `${API_BASE_URL}/api`;
 
 const apiClient = axios.create({
     baseURL: API_URL,
